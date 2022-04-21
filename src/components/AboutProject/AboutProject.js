a1f203ba2881d59acf29c6cb0dfa09922b8fc883
BakayAlexander <bakay.dvr@gmail.com>
2022-04-21 10:18:21 +0300

    feat: add AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -8,18 +8,24 @@ function AboutProject() {
 			<hr className='aboutProject__line' />
 			<div className='aboutProject__text-container'>
 				<div className='aboutProject__text-element'>
-					<p className='aboutProject__text-title'>Дипломный проект включал 5 этапов</p>
+					<h3 className='aboutProject__text-title'>Дипломный проект включал 5 этапов</h3>
 					<p className='aboutProject__text-subtitle'>
 						Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
 					</p>
 				</div>
 				<div className='aboutProject__text-element'>
-					<p className='aboutProject__text-title'>На выполнение диплома ушло 5 недель</p>
+					<h3 className='aboutProject__text-title'>На выполнение диплома ушло 5 недель</h3>
 					<p className='aboutProject__text-subtitle'>
 						У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
 					</p>
 				</div>
 			</div>
+			<ul className='aboutProject__scale'>
+				<li className='aboutProject__duration'>1 неделя</li>
+				<li className='aboutProject__duration aboutProject__duration_frontend'>4 недели</li>
+				<li className='aboutProject__dev-type'>Back-end</li>
+				<li className='aboutProject__dev-type'>Front-end</li>
+			</ul>
 		</section>
 	);
 }
